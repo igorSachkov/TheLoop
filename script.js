a8cecf7e08fdd9092e0f0d7b3c9c7847b47c0574
igorSachkov <igordadadadanet@yandex.ru>
2021-06-12 18:12:53 +0300

    added hamburger

---
+++ script.js
@@ -0,0 +1,19 @@
+const hamburgerButton = document.querySelector('#hamburger-btn');
+const menuBox = document.querySelector(".menu-box")
+
+function hamburgerHandler() {
+    if(hamburgerButton.className === "hamburger-btn__icon_activated") {
+        hamburgerButton.className = "hamburger-btn__icon"
+        menuBox.style.top = "-400px"
+    } else {
+        hamburgerButton.className = "hamburger-btn__icon_activated";
+        menuBox.style.top = "0px"
+    }
+}
+window.addEventListener("resize", ()=> {
+    if(window.innerWidth >= 861) {
+        hamburgerButton.className = "hamburger-btn__icon"
+        menuBox.style.top = "-400px"
+    }
+})
+hamburgerButton.addEventListener("click", hamburgerHandler)
